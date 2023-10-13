1fd284328747c78a07470dbec807167a88c4cf8d
Ariel <arielmalagutti.job@gmail.com>
2023-10-12 23:30:35 -0300

    feat: add create new list feature

--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
+import { TodoList } from '@models/index';
 
-type TodoList = {
-  icon?: string;
-  name: string;
-  todos?: [];
-};
+import { todos } from '@mocks/todos';
 
 @Component({
   selector: 'app-side-bar',
@@ -12,9 +9,35 @@ type TodoList = {
   styleUrls: ['./side-bar.component.css'],
 })
 export class SideBarComponent {
-  todoLists: TodoList[] = [
-    {
-      name: 'Home',
-    },
-  ];
+  todoLists: TodoList[] = todos;
+  newList?: TodoList;
+
+  isEditing: boolean = false;
+  createNewList() {
+    this.isEditing = true;
+  }
+
+  handleNewList(inputValue: string) {
+    this.newList = {
+      id: String(this.todoLists.length + 1),
+      name: inputValue,
+      todos: [],
+    };
+    try {
+      if (!this.newList) throw new Error('Object is empty!');
+      this.todoLists.push(this.newList);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  onBlur() {
+    this.isEditing = false;
+  }
+
+  onEdit() {}
+
+  onDelete() {}
+
+  onRemoveAllTodos() {}
 }
