d105f1cf8830303b4da61b70c88f3d24ecadf637
Ariel <arielmalagutti.job@gmail.com>
2023-10-12 21:29:50 -0300

    feat: add Todo and TodoList models

---
+++ src/models/todo.ts
@@ -0,0 +1,17 @@
+export type TodoList = {
+  icon?: string;
+  id: string;
+  name: string;
+  todos?: Todo[];
+};
+
+export type Todo = {
+  id: string;
+  todoListId: string;
+
+  isComplete: boolean;
+  description: string;
+
+  createdAt: string;
+  completedAt?: string;
+};
