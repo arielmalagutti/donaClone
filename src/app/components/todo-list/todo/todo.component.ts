0066ff4e842a60a47efbe6c5ca14569060dcc77f
Ariel <arielmalagutti.job@gmail.com>
2023-10-12 23:17:39 -0300

    fix: todoList checkbox and value

--- src/app/components/todo-list/todo/todo.component.ts
+++ src/app/components/todo-list/todo/todo.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Todo } from '@models/index';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
 })
 export class TodoComponent {
+  @Input() todo?: Todo;
 
+  isEditing?: boolean = false;
+
+  onComplete(id: Todo['id']) {}
+
+  onDelete(id: Todo['id']) {}
+
+  onEdit(id: Todo['id']) {}
+
+  log(input: any) {
+    console.log(input.checked);
+  }
 }
