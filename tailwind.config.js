43cb299277ec6afbcae39eba441b688b088b35c4
Ariel <arielmalagutti.job@gmail.com>
2023-10-13 09:49:12 -0300

    feat: add Inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
@@ -13,6 +15,10 @@ module.exports = {
           red: "#FF0000",
           yellow: "#ecbc00",
         },
+      },
+
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
       },
     },
   },
